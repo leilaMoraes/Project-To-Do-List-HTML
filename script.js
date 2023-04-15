7eb44e7ddb8a227f819085074e5ee625ed98d528
Leila Moraes <leila.violino@gmail.com>
2023-04-15 02:33:13 -0300

    feat: retira background quando outro item é clicado

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const inputToDo = document.getElementById('texto-tarefa');
 const btnCreate = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+let currentTask;
 
+// eslint-disable-next-line func-names
 window.onload = function () {
-  function createTask() {
-    const newTask = document.createElement('li');
+  function paintTask({ target }) {
+    if (currentTask) {
+      currentTask.classList.remove('tasks');
+    }
+    currentTask = target;
+    currentTask.classList.add('tasks');
+  }
+
+  taskList.addEventListener('click', paintTask);
+};
+
+function createTask() {
+  const newTask = document.createElement('li');
+  if (inputToDo.value === '') alert('insert task');
+  else {
     newTask.innerText = inputToDo.value;
     taskList.appendChild(newTask);
     inputToDo.value = '';
     const id = taskList.getElementsByTagName('li').length;
     newTask.id = `task-${id}`;
-    newTask.addEventListener('click', () => {
-      newTask.classList.add('tasks');
-    });
   }
+}
 
-  btnCreate.addEventListener('click', createTask);
-};
+btnCreate.addEventListener('click', createTask);
