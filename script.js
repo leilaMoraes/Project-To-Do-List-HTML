0ebea6ede53a89070ed310e01f4e9c89a070e0ff
Leila Moraes <leila.violino@gmail.com>
2023-04-15 01:36:54 -0300

    feat: adiciona backgroung ao selecionar elemento da lista

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const inputToDo = document.getElementById('texto-tarefa');
 const btnCreate = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 
-function createTask() {
-  const newTask = document.createElement('li');
-  newTask.innerText = inputToDo.value;
-  taskList.appendChild(newTask);
-  inputToDo.value = '';
-}
+window.onload = function () {
+  function createTask() {
+    const newTask = document.createElement('li');
+    newTask.innerText = inputToDo.value;
+    taskList.appendChild(newTask);
+    inputToDo.value = '';
+    const id = taskList.getElementsByTagName('li').length;
+    newTask.id = `task-${id}`;
+    newTask.addEventListener('click', () => {
+      newTask.classList.add('tasks');
+    });
+  }
 
-btnCreate.addEventListener('click', createTask);
+  btnCreate.addEventListener('click', createTask);
+};
